8c21d08de3b6d486ad8afc7caa72591452ded0b6
NguyenJin <hoanganh27112002@gmail.com>
2026-05-13 15:27:03 +0700

    update BackEnd 13/5/2026

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import cookieParser from "cookie-parser";
 
-
 import { connectDB } from "./lib/db.js";
+
 
 dotenv.config();
 const app = express();
@@ -14,6 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
     console.log('Server is running on PORT: ' + PORT);
